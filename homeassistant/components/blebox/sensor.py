202f342b83f9fc2397ee8ddf6ca1bf3299eceff6
Piotr Paczynski <ppaczyn@gmail.com>
2025-12-12 20:41:47 +0100

    Add state_class to forwardActiveEnergy and reverseActiveEnergy sensors

--- homeassistant/components/blebox/sensor.py
+++ homeassistant/components/blebox/sensor.py
@@ -74,11 +74,13 @@ SENSOR_TYPES = (
         key="forwardActiveEnergy",
         device_class=SensorDeviceClass.ENERGY,
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
     ),
     SensorEntityDescription(
         key="reverseActiveEnergy",
         device_class=SensorDeviceClass.ENERGY,
         native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
+        state_class=SensorStateClass.TOTAL_INCREASING,
     ),
     SensorEntityDescription(
         key="reactivePower",
